641b9e7e7174c8903ad9f5fd8d9cd00950c97d15
George Daniel <dang33zz@gmail.com>
2017-09-08 08:55:25 +0100

    Add email newsletter

---
+++ resources/views/newsletter.blade.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="css/main.css">
+  <title>NewsLetter</title>
+</head>
+<body class="newsletter">
+  <header>
+    <div class="brand-logo">
+      <img src="images/logo-3x.png" alt="">
+    </div>
+  </header>
+  <main>
+    <div class="container">
+      <div class="newsletter-board">
+        <div class="interest">
+            <h2>Interest Accepted!</h2>
+            <p>Dear Yesimama (B1967453),</p>
+           <div>
+              <img src="images/newsletter.jpg" class="img img-circle" alt="">
+           </div>
+            <h2>Ankit (G268375)</h2>
+            <ul class="interest-profile">
+              <li>39 Yrs, 5 Ft 8 In / 173 Cms</li>
+              <li>Jain : Unspecified</li>
+              <li>Location: Atlanta,United States of America</li>
+              <li>Education: B.Sc IT/ Computer Science</li>
+              <li>Occupation: Software Professional</li>
+            </ul>
+            <a href="#">View full profile</a>
+        </div>
+        <div class="last-activity">
+          <div class="header">
+            <h2>Last Activity</h2>
+          </div>
+          <div class="body">
+            <ul class="latest-activities">
+              <li class="activity">
+                <div class="activity__image">
+                  <img class="img img-responsive" src="images/bitmap.jpg">
+                </div>
+                <div class="activity__content">
+                  <p><a href="#">Dele Asiwaju</a>, (34yrs, 178cm) accepted to view your Profile</p>
+                  <ul>
+                    <li><a href="#">Send Interest </a></li>
+                    <li><a href="#">View Profile</a></li>
+                    <li><a href="#">Upgrade to view profile</a></li>
+                  </ul>
+                </div>
+              </li>
+              <li class="activity">
+                <div class="activity__image">
+                  <img class="img img-responsive" src="images/bitmap.jpg">
+                </div>
+                <div class="activity__content">
+                  <p><a href="#">Dele Asiwaju</a>, (34yrs, 178cm) accepted to view your Profile </p>
+                  <ul>
+                    <li><a href="#">Send Interest </a></li>
+                    <li><a href="#">View Profile</a></li>
+                    <li><a href="#">Upgrade to view profile</a></li>
+                  </ul>
+                </div>
+              </li>
+              <li class="activity">
+                <div class="activity__image">
+                  <img class="img img-responsive" src="images/bitmap.jpg">
+                </div>
+                <div class="activity__content">
+                  <p><a href="#">Dele Asiwaju</a>, (34yrs, 178cm) accepted to view your Profile </p>
+                  <ul>
+                    <li><a href="#">Send Interest </a></li>
+                    <li><a href="#">View Profile</a></li>
+                    <li><a href="#">Upgrade to view profile</a></li>
+                  </ul>
+                </div>
+              </li>
+            </ul>
+          </div>
+          <a href="#">Upgrade Now</a>
+          <div class="footer">
+            <p>
+              Team - LostRib For Happy Marriages
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+  <footer class="newsletter-footer">
+    <div class="container tc">
+      <p>
+         You are a BharatMatrimony.com member. This e-mail comes to you in accordance with BharatMatrimony.com's Privacy Policy.
+        Click here to unsubscribe. BharatMatrimony.com is not responsible for content other than its own and makes no warranties
+        or guarantees about the products or services that are advertised.
+      </p>
+      <div class="address">
+         Product Hunt Inc., 277 Golden Gate Ave, Apt 404, San Francisco, CA 94102
+      </div>
+      <a href="#"><i class="fa fa-facebook"></i></a>
+      <a href="#"><i class="fa fa-twitter"></i></a>
+      <a href="#"><i class="fa fa-pinterest-p"></i></a>
+    </div>
+  </footer>
+</body>
+</html>
