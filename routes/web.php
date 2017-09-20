b358af1dfc9ab917f20b1c22d8297de7f24ac604
Pens <pensnaku@gmail.com>
2017-09-20 08:53:30 +0100

    login

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function(){
-    return view('keywordsearch');
+    return view('advancedsearch');
 });
 
 Route::get('/', 'Controller@index')->name('index_page');
